a5b05e3476d03491e5f9cf52bb08a49bffcc9a1b
jankwa <halienovajana@gmail.com>
2024-11-12 15:50:09 +0100

    Upd.2 - Moving Character
    - move() done - player class changed
    - draw_hud() done
    - run_game() - edited keys, spawned player

--- base.py
+++ base.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 from sys import exit
 
 # SYSTEM
-FPS = 30
+FPS = 60
 SCREEN_WIDTH = 1080
 SCREEN_HEIGHT = 720
 COIN_SIZE = 20
@@ -26,24 +26,35 @@ COIN_COLOR = YELLOW
 CASH_COLOR = GREEN
 
 # CHARACTER
-CHARACTER_COLOR = RED
+CHARACTER_COLOR = BLACK
 CHARACTER_SIZE = 50
-PLAYER_SPEED = 10
+PLAYER_SPEED = 12
 PLAYER_POSITION = [SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2]
 HP = 3
 POINTS = 0
 
+# MINI GAMES - changeable
+MINIGAME_TIME_LIMIT = 20  # 20 SEKUND
+MINIGAME_ACTIVE = False
+
 
 class Player:
 
-    def __init__(self, position: list, speed: int, hp: int, points: int):
+    def __init__(self, position: list):
         self.pos = position
-        self.speed = speed
-        self.hp = hp
-        self.points = points
 
     def __str__(self):
-        return str(f"HP: {self.hp}, POINTS: {self.points}")
+        return str(self.pos)
+    
+    def move(self, direction):
+        if direction == 'up' and self.pos[1] > 0:
+            self.pos[1] -= PLAYER_SPEED
+        elif direction == 'down' and self.pos[1] < SCREEN_HEIGHT - CHARACTER_SIZE:
+            self.pos[1] += PLAYER_SPEED
+        elif direction == 'left' and self.pos[0] > 0:
+            self.pos[0] -= PLAYER_SPEED
+        elif direction == 'right' and self.pos[0] < SCREEN_WIDTH - CHARACTER_SIZE:
+            self.pos[0] += PLAYER_SPEED
 
 
 class Coin:
@@ -58,10 +69,10 @@ SYSTEM FUNC
 
 # MAIN
 def main():
-    global FPS_CLOCK, DISPLAY_SURF, BASIC_FONT
+    global FPS_CLOCK, SCREEN, BASIC_FONT
     pygame.init()
     FPS_CLOCK = pygame.time.Clock()
-    DISPLAY_SURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     BASIC_FONT = pygame.font.Font('freesansbold.ttf', 18)
     pygame.display.set_caption('Catch Your Paycheck!')
 
@@ -95,7 +106,7 @@ GAME FUNC
 # GENEROVANIE HRACA NA ZACIATKU
 def get_new_character():
     # TO DO
-    return Player(PLAYER_POSITION, PLAYER_SPEED, HP, POINTS)
+    return Player(PLAYER_POSITION)
 
 # GENEROVANIE NAHODNEJ POZICIE COINU
 def random_coin_pos():
@@ -104,7 +115,11 @@ def random_coin_pos():
 
 # ZOBRAZENIE STAVU HRACA - HP, POINTS
 def draw_hud():
-    pass
+    font = pygame.font.Font(None, 36)
+    hp_text = font.render(f"HP: {HP}", True, BLACK)
+    points_text = font.render(f"Score: {POINTS}", True, WHITE)
+    SCREEN.blit(hp_text, (10, 10))
+    SCREEN.blit(points_text, (10, 50))
 
 
 '''
@@ -127,17 +142,30 @@ GAME LOOP
 def run_game():
     player = get_new_character()
     coin = random_coin_pos()
-    direction = " "
+    direction = ""
 
-    while True: # MAIN GAME LOOP
-        DISPLAY_SURF.fill(BG_COLOR)
+    while True:
+        SCREEN.fill(BG_COLOR)
         EVENTS = pygame.event.get()
-        for event in EVENTS: # EVENTY
+        for event in EVENTS:  # EVENTS
             if event.type == QUIT:
                 end_game()
             elif event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     end_game()
+
+        keys = pygame.key.get_pressed()
+        if keys[K_UP]:
+            player.move('up')
+        elif keys[K_DOWN]:
+            player.move('down')
+        elif keys[K_LEFT]:
+            player.move('left')
+        elif keys[K_RIGHT]:
+            player.move('right')
+
+        pygame.draw.rect(SCREEN, CHARACTER_COLOR, (player.pos[0], player.pos[1], CHARACTER_SIZE, CHARACTER_SIZE))
+        draw_hud()
         pygame.display.update()
         FPS_CLOCK.tick(FPS)
 
